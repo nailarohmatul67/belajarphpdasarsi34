58d8f473b2b663602cc697efe87fe89b97dce55a
nailarohmatul67 <nailarohmatul867@gmail.com>
2021-02-25 15:41:21 +0700

    belajar php dasar

---
+++ belajar.php
@@ -0,0 +1,15 @@
+<?php
+
+    $nama = 'Naila';
+    $angka = 11;
+
+    echo "nama saya adalah : ".$nama;
+    echo "<br>";
+
+
+    echo "hello world!";
+    echo "<br>";
+
+    echo "dokumen :".$_SERVER["DOCUMENT_ROOT"];
+
+?>
